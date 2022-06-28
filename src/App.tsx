57f8350b7c299ee6b540ab15fbf90e9036efff78
Thiago Saraiva <saraii_@live.com>
2022-06-28 17:30:09 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { PokemonCard } from './components/Pokemon/PokemonCard'
+import { Header } from './components/Header'
+
+function App() {
+  return (
+    <> 
+      <Header />
+      <PokemonCard/>
+    </>
+    
+  )
+}
+
+export default App
